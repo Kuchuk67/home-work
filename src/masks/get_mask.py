4377dfbc126abe236b21c5680212ffd3b88bb6e9
Kuchuk67 <kuchukov.sergey@gmail.com>
2024-09-03 13:18:48 +0300

    add2

--- src/masks/get_mask.py
+++ src/masks/get_mask.py
@@ -8,17 +8,11 @@ def get_mask_card_number(card_number: int) -> str:
     return "{0} {1}** **** {2}".format(card_number_str[0:4], card_number_str[4:6], card_number_str[-4:])
 
 
-
-
-
 def get_mask_account(account_number: int) -> str:
     """Функция маскировки номера банковского счета
     принимает на вход номер счета и возвращает его маску
     73654108430135874305  # входной аргумент
-    **4305  # выход функции     """
+    **4305  # выход функции"""
 
     account_number_str = str(account_number)
     return f"**{account_number_str[-4:]}"
-
-
-
