61ce7cfe5d348a3d360e68d9a667738921a70903
PC <kuchukov.sergey@gmail.com>
2024-09-03 18:30:25 +0300

    add

--- tests/get_mask.py
+++ tests/get_mask.py
@@ -31,6 +31,6 @@ def get_mask_account(account_number: int) -> str:
 
 
 
-## для проверки
+# для проверки
 print(get_mask_card_number(7000792289606361))
 print(get_mask_account(73654108430135874305))
