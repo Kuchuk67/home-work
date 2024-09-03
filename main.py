8f0a74f76ee653ce80c4e90923980c6df0f8b82d
Kuchuk67 <kuchukov.sergey@gmail.com>
2024-09-03 12:19:49 +0300

    add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask
 
-#import src.masks.get_mask
+## для проверки
 print(get_mask.get_mask_card_number(7000792289606361))
 print(get_mask.get_mask_account(73654108430135874305))
