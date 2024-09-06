60e1c6790a305294124e5dbbc96e4e525ac6418e
Serg Kuchukov Home <kuchukov.sergey@gmail.com>
2024-09-06 07:08:42 +0300

    Исправдено согластно PEP8

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from src import masks
 from src import widget
 
 # для проверки
-print(masks.get_mask_card_number(7000792289606361))
-print(masks.get_mask_account(73654108430135874305))
 
 print(widget.mask_account_card('Visa Platinum 7000792289606361'))
 print(widget.mask_account_card('Счет 73654108430135874305'))
