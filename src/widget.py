c39712e7ea1d27c7711656835649fc5aa039adb9
Serg Kuchukov Home <kuchukov.sergey@gmail.com>
2024-09-06 07:35:36 +0300

    Добавлена функция get_date

--- src/widget.py
+++ src/widget.py
@@ -15,3 +15,11 @@ def mask_account_card(customer_card: str) -> str:
     customer_card_list.append(mask_number)
     mask_customer_card = " ".join(customer_card_list)
     return mask_customer_card
+
+
+def get_date(date_format_full: str) -> str:
+    """возвращает строку с датой в формате  "ДД.ММ.ГГГГ"
+    ввод  2024-03-11T02:26:18.671407"""
+
+    date_format_dd_mm_yyyy = f"{date_format_full[5:7]}.{date_format_full[8:10]}.{date_format_full[:4]}"
+    return date_format_dd_mm_yyyy
