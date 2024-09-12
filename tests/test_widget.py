8880c984dddfd2f5b4caf978b7f1be760c070381
Kuchuk67 <kuchukov.sergey@gmail.com>
2024-09-12 22:08:06 +0300

    fixed function widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -14,10 +14,10 @@ from src.widget import mask_account_card
                                               ('Счет 35383033474447895560', 'Счет **5560'),
                                               ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
                                               ('Visa  Classic  6831982476737658', 'Visa Classic 6831 98** **** 7658'),
-                                              ('MasterCard 7158R300734726758', 'Error'),
+                                              ('MasterCard 7158R300734726758', 'MasterCard 7158 30** **** 6758'),
                                               ('MasterCard 34 7158300734726758', 'MasterCard 34 7158 30** **** 6758'),
                                               ('', 'Error'),
-                                              ('MasterCard', 'Error'),
+                                              ('Master Card', 'Error'),
                                               ('343254544354', 'Error'),
                                               ])
 def test_mask_account_card(number, expected):
