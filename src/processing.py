f9410f83c5b0f413e43c02e7f27833dc381b7028
Kuchuk67 <kuchukov.sergey@gmail.com>
2024-09-12 23:29:37 +0300

    fixed function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,7 @@ def filter_by_state(list_for_filter: list, key_state: str = "EXECUTED") -> list:
     """Функция  возвращает  список словарей,  у которых ключ state соответствует указанному значению.
     ключ state по умолчанию 'EXECUTED'
     filter_by_state(list_dict, state)"""
-    return [dict_from_list for dict_from_list in list_for_filter if dict_from_list["state"] == key_state]
+    return [dict_from_list for dict_from_list in list_for_filter if dict_from_list.get("state") == key_state]
 
 
 def sort_by_date(list_for_sort: list, reverse_sort: bool = True) -> list:
@@ -12,3 +12,5 @@ def sort_by_date(list_for_sort: list, reverse_sort: bool = True) -> list:
      False по возрастанию
     sort_by_date(list_for_sort, False)"""
     return sorted(list_for_sort, key=lambda x: x.get("date"), reverse=reverse_sort)
+
+
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,20 +1,24 @@
 import pytest
+
+
 # Создаем фикстуру, которая запускается перед каждым тестом
 @pytest.fixture
-def state(): # Имя фикстуры — любое
+def state():  # Имя фикстуры — любое
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},]
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, ]
+
+
 @pytest.fixture
-def no_state(): # Имя фикстуры — любое
-    return [{'id': 41428829,  'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},]
-#@pytest.fixture
-#def expected_for_state(): # Имя фикстуры — любое
-    #return \
+def no_state():  # Имя фикстуры — любое
+    return [{'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, ]
+
+
+
 
 """
 Функция filter_by_state:
@@ -24,18 +28,18 @@ def no_state(): # Имя фикстуры — любое
 """
 from src.processing import filter_by_state
 
+
 def test_filter_by_no_state(no_state):
     assert filter_by_state(no_state, 'TEST') == []
 
 
-
 @pytest.mark.parametrize("state_value, expected", [('CANCELED', [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        ]),('EXECUTED', [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        ]),])
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+]), ('EXECUTED', [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+]), ])
 def test_filter_by_state_value(state, state_value, expected):
     assert filter_by_state(state, state_value) == expected
 
@@ -47,24 +51,28 @@ def test_filter_by_state_value(state, state_value, expected):
 Тесты на работу функции с некорректными или нестандартными форматами дат.
 """
 from src.processing import sort_by_date
+
 expected_for_sort_asc = [
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    ]
+]
 expected_for_sort_desc = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    ]
+]
+
 
 def test_sort_by_date_empty(state):
-    assert sort_by_date(state ) == expected_for_sort_desc
+    assert sort_by_date(state) == expected_for_sort_desc
+
 
 def test_sort_by_date_true(state):
-    assert sort_by_date(state, True ) == expected_for_sort_desc
+    assert sort_by_date(state, True) == expected_for_sort_desc
+
 
 def test_sort_by_date_false(state):
-    assert sort_by_date(state, False ) == expected_for_sort_asc
+    assert sort_by_date(state, False) == expected_for_sort_asc
