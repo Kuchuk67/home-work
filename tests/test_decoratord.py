58afee8e8c332d179af1c682e453588d6d39f986
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2024-09-19 09:35:06 +0300

    updata test_decorators, add auto сreation directiry  log

--- tests/test_decoratord.py
+++ tests/test_decoratord.py
@@ -1,14 +1,24 @@
+from typing import Any
+
 import pytest
+
 from src.decorators import log
 
-def test_log(capsys):
-    @log(filename = '')
-    def add_numbers(a):
+
+def test_log(capsys: Any) -> None:
+    @log(filename="")
+    def add_numbers(a: int) -> float:
         return 10 / a
 
     result = add_numbers(5)
 
     captured = capsys.readouterr()
-    assert captured.out == 'add_numbers Ok\n\n'
+    assert captured.out == "add_numbers Ok\n"
     assert result == 2
 
+    with pytest.raises(ZeroDivisionError) as excinfo:
+        add_numbers(0)
+
+    assert str(excinfo.value) == 'division by zero'
+    captured2 = capsys.readouterr()
+    assert captured2.out == "add_numbers error: division by zero. Inputs: (0,) {}\n"
