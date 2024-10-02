b8a800258e5c1ba4dace9b9662d5ffd31b900ea0
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2024-10-02 15:34:22 +0300

    add test_utils

---
+++ tests/test_utils.py
@@ -0,0 +1,44 @@
+from unittest.mock import mock_open, patch
+from src.utils import read_json_file
+
+data_json = '''[
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
+]'''
+data_list = [{
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+        "amount": "31957.58",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+}]
+
+
+# Mocking file open in Python
+
+def test_read_json_file() -> None:
+    m = mock_open(read_data=data_json)
+    with patch("builtins.open", m):
+        lines = read_json_file("my_file.txt")
+    assert lines == data_list
